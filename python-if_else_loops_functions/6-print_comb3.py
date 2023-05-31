92b2e2b320bbd52572d4f707381240a23f24632d
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-05-31 15:26:15 +0200

    print comb3

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for digit_one in range(0, 10):
+    for digit_two in range(digit_one + 1, 10):
+        if digit_one == 8 and digit_two == 9:
+            print("{}{}".format(digit_one, digit_two))
+        else:
+            print("{}{}".format(digit_one, digit_two), end=", ")
