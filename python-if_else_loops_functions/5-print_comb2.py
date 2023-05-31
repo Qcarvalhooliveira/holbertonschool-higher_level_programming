69be8b1beeb7be098f38b75f85446adb9ef3dcb3
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-05-31 15:00:26 +0200

    print comb2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -4,4 +4,4 @@ for numbers in range(0, 100):
     if numbers == 99:
         print("{}".format(numbers))
     else:
-        print("{:02}".format(numbers), end=",")
+        print("{:02}".format(numbers), end=", ")
