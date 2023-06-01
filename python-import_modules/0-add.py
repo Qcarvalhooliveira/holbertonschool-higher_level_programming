8b2670246cf05b14e2d04a7b3af793b44b30ba2c
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-06-01 20:56:26 +0200

    fixed error function add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import add_0
-
-result = add_0.add(1, 2)
-print("{a} + {b} = {result}".format(a=1, b=2, result=result))
+a = 1
+b = 2
+result = add_0.add(a, b)
+print("{a} + {b} = {result}".format(a=a, b=b, result=result))
