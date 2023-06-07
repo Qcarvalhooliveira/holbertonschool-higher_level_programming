66b46c7a8f306667d9ca4e6218033237ff40830c
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-06-07 23:30:44 +0200

    print_sorted_dictionary

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    ordered_keys = sorted(a_dictionary.keys())
+    for keys in ordered_keys:
+        value = a_dictionary[keys]
+        print(keys, value)
+
