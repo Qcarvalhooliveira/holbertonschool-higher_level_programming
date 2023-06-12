542ca331d995d209fd2875488ab20c88ce6cc1b1
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-06-12 23:50:47 +0200

    defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """size: length of side of the square"""
+        self.__size = size
