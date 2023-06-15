cb490e8d8c4e5fe395b2ac1f84647b55155c85d4
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-06-15 21:55:43 +0200

    say_my_name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,11 @@
 
 def say_my_name(first_name, last_name=""):
     """
+    Prints my name
+
+    Raises:
+        TypeError:first_name must be a string
+        TypeError:last_name must be a string
     """
     if not isinstance(first_name, (str)):
         raise TypeError("first_name must be a string")
