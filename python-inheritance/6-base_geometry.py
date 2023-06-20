c0292111f221ad2a041e4fdff5d8a0ba25e3e149
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-06-20 22:51:05 +0200

    base_geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module to Write an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Determines BaseGeometry class"""
+    def area(self):
+        raise Exception("area() is not implemented")
