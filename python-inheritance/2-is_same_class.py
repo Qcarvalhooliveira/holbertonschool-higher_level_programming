5660b2424ae0b41691ddf5efac970bbc87b6fe2b
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-06-20 20:47:48 +0200

    is_same_class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Determines if an object is exactly an instance of a class"""
+"""Module to returns True if the object is exactly an instance"""
 
 
 def is_same_class(obj, a_class):
+    """Determines if an object is exactly an instance of a class"""
     return issubclass(a_class, type(obj))
