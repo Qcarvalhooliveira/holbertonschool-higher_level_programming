e1ffb1c3d6e154071aa970eb8fc3f63d99c8071a
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-06-23 11:21:53 +0200

    to_json_string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for to_json_string method"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object"""
+    return (json.dumps(my_obj))
