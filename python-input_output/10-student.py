b254ebfa99eeecdfdcff7df3869b150e6d8e7695
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-06-23 16:12:08 +0200

    student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module for class Student"""
+
+
+class Student:
+    """Class Student that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialises Student instance"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
