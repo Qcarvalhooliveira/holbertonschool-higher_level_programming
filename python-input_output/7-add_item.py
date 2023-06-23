81cf038ab46c2b150ad3d3ea32b45378591304c4
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-06-23 14:16:58 +0200

    add_item

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module for add_item method"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""script that adds all arguments to a Python list"""
+
+import json
+import sys
+import os.path
+
+file = "add_item.json"
+json_list = []
+
+if os.path.exists(file):
+    json_list = load_from_json_file(file)
+
+for i in range(1, len(sys.argv)):
+    json_list.append(sys.argv[i])
+
+save_to_json_file(json_list, file)
+
+
