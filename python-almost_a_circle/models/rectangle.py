c66718f4c4a2d49100d08239822c08a151bd1c7a
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-06-27 21:31:33 +0200

    improving the public method display taking care of x and y

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -75,8 +75,10 @@ class Rectangle(Base):
 
     def display(self):
         """Prints the Rectangle instance with the character '#'"""
+        for empty_line in range(self.__y):
+            print()
         for line in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """Return representation of string"""
