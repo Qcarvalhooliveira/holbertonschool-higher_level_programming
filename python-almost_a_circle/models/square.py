3094aab3d126d3318389134243946ce9f73028f9
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-06-28 13:59:15 +0200

    adding the public method def update(self, *args, **kwargs)

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -24,3 +24,17 @@ class Square(Rectangle):
         """Setter function for public attribute size"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Assigns key/value arguments to attributes"""
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.size = args[1]
+        if len(args) > 2:
+            self.x = args[2]
+        if len(args) > 3:
+            self.y = args[3]
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
