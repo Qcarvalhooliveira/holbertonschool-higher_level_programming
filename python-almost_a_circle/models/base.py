e109ffdc3d0a988cbdc602b2eb5e18e8962f5973
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-06-28 23:14:31 +0200

    adding the class method def create(cls, **dictionary)

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -45,3 +45,16 @@ class Base:
             return []
         else:
             return loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = cls()
+
+        dummy.update(**dictionary)
+        return dummy
