d6bee31003ae7fe2772a25faccb01894c41c0312
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-25 11:20:47 +0200

    First add

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- Inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
 -- The database name will be passed as an argument of the mysql command.
-INSERT INTO first_table (`id`, `name`) VALUES (89, Best School);
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
