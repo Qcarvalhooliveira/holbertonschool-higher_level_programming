45c5875000473549582408f51ee80718cd8008cd
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-25 13:52:27 +0200

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 -- The database name will be passed as an argument to the mysql command.
 -- If the table second_table already exists, your script should not fail.
 CREATE TABLE IF NOT EXISTS `second_table`(`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO second_table (id, name, score) VALUES (1, “John”, 10);
-INSERT INTO second_table (id, name, score) VALUES (2, “Alex”, 3);
-INSERT INTO second_table (id, name, score) VALUES (3, “Bob”, 14);
-INSERT INTO second_table (id, name, score) VALUES (4, “George”, 8);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
