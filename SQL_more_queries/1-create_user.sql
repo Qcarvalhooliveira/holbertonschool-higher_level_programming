393c89a041d1103ce2f340d533ad391e1a8f4d76
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-26 14:51:02 +0200

    Root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -4,4 +4,4 @@
 -- If the user user_0d_1 already exists, your script should not fail.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
