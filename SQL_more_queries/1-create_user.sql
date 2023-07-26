3106a6981b5e4ec1c8bd160bdf51d832028b52e5
Qcarvalhooliveira <124299955+Qcarvalhooliveira@users.noreply.github.com>
2023-07-26 15:20:41 +0200

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,6 +2,6 @@
 -- User_0d_1 should have all privileges on your MySQL server.
 -- The user_0d_1 password should be set to user_0d_1_pwd.
 -- If the user user_0d_1 already exists, your script should not fail.
-CREATE USER IF NOT EXISTS user_0d_1'@'localhost;
-SET PASSWORD FOR user_0d_1'@'localhost = 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1'@'localhost;
+CREATE USER IF NOT EXISTS user_0d_1@localhost;
+SET PASSWORD FOR user_0d_1@localhost = 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
