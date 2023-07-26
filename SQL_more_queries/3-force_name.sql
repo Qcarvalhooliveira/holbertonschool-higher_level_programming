ce6a89983b4f43b34812435e66bd7ded7c01fc37
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-07-26 23:40:32 +0200

    fixed Always a name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Creates the table force_name on your MySQL server.
 -- The database name will be passed as an argument of the mysql command.
---If the table force_name already exists, your script should not fail.
+-- If the table force_name already exists, your script should not fail.
 CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
 
