acd1b4cd118ac408775ea2431f2de57d494105c6
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-08-02 17:41:03 +0200

    Update a state

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module that adds the State object “Louisiana” to the database"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3],
+                                  pool_pre_ping=True))
+    """Creating a connection to the MySQL server"""
+
+    Session = sessionmaker(bind=engine)
+    """Creating a session to interact with the database"""
+    session = Session()
+
+    session.query(State).filter(State.id == 2).update({"name": "New Mexico"})
+
+    session.commit()
+
+    session.close()
+    """Close the session"""
