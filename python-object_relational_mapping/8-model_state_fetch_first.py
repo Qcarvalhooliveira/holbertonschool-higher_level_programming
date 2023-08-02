e136499a22c173e330796cf0be9db0fc7e1a0a82
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-08-02 10:55:44 +0200

    First state

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that lists all State objects from the database hbtn_0e_6_usa"""
+"""Module that prints the first State object from the database hbtn_0e_6_usa"""
 
 from sys import argv
 from model_state import Base, State
