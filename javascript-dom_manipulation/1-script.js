f31d71ed4491e88e103ae9032568fe532c24b5bc
Qcarvalhooliveira <124299955+Qcarvalhooliveira@users.noreply.github.com>
2023-09-11 22:55:07 +0200

    Click and turn red

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,6 @@
+let headerElement = document.querySelector('header'); // select the header element
+
+
+red_header.addEventListener('click', () => {
+    headerElement.style.color = '#FF0000';
+    }); // added event listener to the red_header id to change the color of the header element on click
