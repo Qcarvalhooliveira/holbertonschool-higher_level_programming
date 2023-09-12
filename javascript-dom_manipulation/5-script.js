aca76e503f0a635dece179259aca99225d311db6
Qcarvalhooliveira <124299955+Qcarvalhooliveira@users.noreply.github.com>
2023-09-12 17:58:33 +0200

    Adding comments

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,5 +1,5 @@
-let headerElement = document.getElementById("update_header");
+let headerElement = document.getElementById("update_header"); // Select the element with the id 'update_header'
 
 update_header.addEventListener('click', () => {
      headerElement.textContent="New Header!!!";
-});
+}); // added event listener to the update_header element to update the header text when clicked
