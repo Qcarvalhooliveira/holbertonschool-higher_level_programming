6bdb967cf3672b73ff8f43e104a36cd4897eb452
Qcarvalhooliveira <124299955+Qcarvalhooliveira@users.noreply.github.com>
2023-09-12 17:17:31 +0200

    Update 2-script.js

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,5 +1,5 @@
 let redClass = document.querySelector('#red_header'); // select the id red_header element
 
-redClass.addEventListener('click', () => {
+red_header.addEventListener('click', () => {
     redClass.classList.add('red');
 }); // added event listener to the red_header id to add red class to the header element on click
